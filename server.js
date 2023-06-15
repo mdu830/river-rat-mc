3cf18bac286b02dacf6162a2ecf6245a3e0b37cc
Mitch Duncan <mdu830@Mitchs-MacBook-Pro.local>
2023-06-14 21:45:38 -0400

    fixing locations 3

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static(path.resolve(
 )));
 
 app.get('*', (req,res) => {
-    res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, './client/build'));
 });
 
 app.get("/api", (req, res) => {
