01c037db2831b3a59999ece0b97e756bdb471410
Mitch Duncan <mdu830@Mitchs-MacBook-Pro.local>
2023-06-14 21:20:43 -0400

    delivered app by express

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 // Remember to change "nodemon" to "node" of npm start script(package.json) for Production
+const path = require('path');
 const express = require('express');
+const PORT = process.env.PORT || 3090;
+
 const app = express();
-const PORT = process.env.PORT || 3333;
 
 app.use(express.json());
+
+app.get("/api", (req, res) => {
+    res.json({ message: "hello from rrmc server "});
+});
+
+app.use(express.static(path.resolve(
+    __dirname, './client/build'
+)));
 
 app.listen(PORT, () => {
     console.log(`Server listening on PORT ${PORT}`);
 });
+
