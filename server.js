e81568b54876befddf377af164730cf237a6a76f
Mitch Duncan <mdu830@Mitchs-MacBook-Pro.local>
2023-06-14 21:38:24 -0400

    correcting address res for app 2

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const app = express();
 // app.use(express.json());
 
 app.use(express.static(path.resolve(
-    __dirname, './client/build'
+    __dirname, './client/build/'
 )));
 
 app.get('/', (req,res) => {
-    res.sendFile(path.resolve(__dirname, './client/build/index.html'));
+    res.sendFile(path.resolve(__dirname, './client/build/', 'index.html'));
 });
 
 app.get("/api", (req, res) => {
