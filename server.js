d7c50e5cb145f851b42e89b61a15ff08637df6f3
Mitch Duncan <mdu830@Mitchs-MacBook-Pro.local>
2023-06-14 23:20:04 -0400

    express test 2

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const app = express();
 
 // app.use(express.json());
 
-app.use(express.static(path.resolve(
-    __dirname, './client/build/'
-)));
+// app.use(express.static(path.resolve(
+//     __dirname, './client/build/'
+// )));
 
 app.get("/api", (req, res) => {
     res.json({ message: "hello from rrmc server "});
