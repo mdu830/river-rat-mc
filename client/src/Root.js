15735d2d6d8ad0c7381d627ada1e21e5a59c88d6
Mitch Duncan <mdu830@Mitchs-MacBook-Pro.local>
2023-06-17 11:17:15 -0400

    changed app to root, and set it as /

---
+++ client/src/Root.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Header from './components/header'
+function Root() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default Root;
--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import './assets/style.css'
 import reportWebVitals from './reportWebVitals';
-import Header from './components/header';
-import  HomePage from './routes/HomePage';
+// import Header from './components/header';
+import Root from './Root';
+// import  HomePage from './routes/HomePage';
 import About from './routes/About';
 import Contact from './routes/Contact'
 
@@ -16,7 +17,7 @@ import {
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage />,
+    element: <Root />,
     // errorElement: <ErrorPage />,
     children: [
       {
@@ -33,12 +34,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Header />
     <RouterProvider router={router} />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
