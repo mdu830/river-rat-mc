03b3264b29e32423bf5db463e8aa1ebe8df3c6d7
Mitch Duncan <mdu830@Mitchs-MacBook-Pro.local>
2023-06-18 11:15:28 -0400

    cleaning server, got cyclic sever working with remove build from gitignore

--- server.js
+++ server.js
@@ -1,26 +1,3 @@
-// const path = require('path');
-// const express = require('express');
-// const PORT = process.env.PORT || 3000;
-// const build = './client/build'
-
-// const app = express();
-
-// // dev routes for client
-// app.use(express.static(path.resolve(
-//     __dirname, build
-// )));
-
-// app.get('/*', function (req, res) {
-//     res.sendFile(path.join(__dirname, build, 'index.html'));
-//   });
-
-// // server
-// app.listen(PORT, (err) => {
-//     console.log(`Server listening on PORT ${PORT}`);
-// });
-
-// /server.js
-
 const express = require('express')
 const path = require("path");
 const app = express();
@@ -28,8 +5,6 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-// This configures static hosting for files in /public that have the extensions
-// listed in the array.
 var options = {
   dotfiles: 'ignore',
   etag: false,
@@ -39,12 +14,6 @@ var options = {
   redirect: false
 }
 app.use(express.static('./client/build', options.index))
-
-// console.log(app.use(express.static('client/build', options.index)))
-
-// app.get('*', function (req, res) {
-//   res.sendFile(path.join(__dirname, './client/build/index.html'));
-// });
 
 app.get('*', function (req, res) {
   const index = path.join(__dirname, 'client/build', 'index.html');
