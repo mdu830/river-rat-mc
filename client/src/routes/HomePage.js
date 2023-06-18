1ee66c7d6a1c2d2378cae3fcb7ff31575812afaa
Mitch Duncan <mdu830@Mitchs-MacBook-Pro.local>
2023-06-18 16:39:12 -0400

    added style and logo to homepage

--- client/src/routes/HomePage.js
+++ client/src/routes/HomePage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../assets/style.css'
+import logo from '../assets/images/logo.png'
 
 
 export default function HomePage() {
@@ -7,6 +8,7 @@ export default function HomePage() {
     <>
       {/* all the other elements */}
       <div className="page">
+        <img className="icon" alt="" src={logo} />
         <h1>HomePage</h1>
       </div>
     </>
