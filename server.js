075477852285d8c666919cb8154b6bf5d61c1a15
Mitch Duncan <mdu830@Mitchs-MacBook-Pro.local>
2023-06-18 11:02:47 -0400

    cyclic redeploy, host can't find client/build

--- server.js
+++ server.js
@@ -38,12 +38,12 @@ var options = {
   maxAge: '1m',
   redirect: false
 }
-app.use(express.static('client/build', options.index))
+app.use(express.static('./client/build', options.index))
 
 // console.log(app.use(express.static('client/build', options.index)))
 
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'client/build/index.html'));
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
 const port = process.env.PORT
