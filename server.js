f9f9e1b48c329d8121146a9f45c1af4e7c5ce1a9
Mitch Duncan <mdu830@Mitchs-MacBook-Pro.local>
2023-06-17 23:05:18 -0400

    16 rrmc

--- server.js
+++ server.js
@@ -23,32 +23,31 @@
 
 const express = require('express')
 const path = require("path");
-const app = express()
+const app = express();
+// const dotenv = require("dotenv");
+
+// dotenv.config();
 
 // This configures static hosting for files in /public that have the extensions
 // listed in the array.
 var options = {
   dotfiles: 'ignore',
   etag: false,
-  extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
+  extensions: ['htm', 'html', 'css', 'js', 'ico', 'jpg', 'jpeg', 'png', 'svg'],
   index: ['index.html'],
   maxAge: '1m',
   redirect: false
 }
-// app.use(express.static('client/public', options))
+app.use(express.static('client/build', options.index))
 
-// app.get('/*', function (req, res) {
-//     res.sendFile(path.join(__dirname, 'client/public', 'index.html'));
-//   });
-
-// 
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'client/build', '/index.html'));
+});
 
 const port = process.env.PORT || 3000
 
-// console.log(process.env)
-
 app.listen(port, () => {
 
-  console.log(`React app listening at http://localhost:${port}`)
+  console.log(`Server listening at http://localhost:${port}`)
 })
 // 
