2f0e268467e065004bd7cc433f500a066b44f547
Mitch Duncan <mdu830@Mitchs-MacBook-Pro.local>
2023-06-19 11:17:13 -0400

    testing loading

--- client/src/Root.js
+++ client/src/Root.js
@@ -2,9 +2,9 @@ import './App.css';
 import Header from './components/header'
 import { Outlet } from "react-router-dom";
 import { Spinner } from 'reactstrap';
-import { useEffect, useState } from 'react';
+import { Component, useEffect, useState } from 'react';
 
-function Root() {
+function Root(data) {
 
   const [isLoading, setIsLoading] = useState(true)
 
@@ -14,28 +14,32 @@ function Root() {
       setIsLoading(false);
     };
 
-    (document.readyState === 'complete') ?
-      onPageLoad() : window.addEventListener('load', onPageLoad);
+    (document.readyState === 'complete') 
+    ?
+    onPageLoad() 
+    : 
+    window.addEventListener('load', onPageLoad);
     return () => window.removeEventListener('load', onPageLoad);
 
   }, []);
 
-  console.log(isLoading)
+  console.log(data);
 
-  if (isLoading) {
-  return (
-    <div className='page'>
-      <Spinner id='spinner' color="secondary" />
-    </div>
-  )
-  }
-
-  return (
-    <div className="App">
-      <Header />
-      <Outlet />
-    </div>
-  );
+  return isLoading
+    ?
+    (
+      <div className=''>
+        <Spinner id='spinner' color="secondary" />
+      </div>
+    )
+    :
+    (
+      <div className="App">
+        <Header />
+        <Outlet />
+      </div>
+    )
 }
+
 
 export default Root;
