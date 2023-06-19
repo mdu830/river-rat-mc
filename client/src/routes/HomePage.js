06d6d390d497ed73d9112a94225b124bd3c5290a
Mitch Duncan <mdu830@Mitchs-MacBook-Pro.local>
2023-06-18 20:01:50 -0400

    added dev message to homepage

--- client/src/routes/HomePage.js
+++ client/src/routes/HomePage.js
@@ -9,7 +9,9 @@ export default function HomePage() {
       {/* all the other elements */}
       <div className="page">
         <img className="icon" alt="" src={logo} />
-        <h1>HomePage</h1>
+        <h2>Welcome to River Rat Marine Construction! 
+          Our website is currently under development. 
+        </h2>
       </div>
     </>
   );
