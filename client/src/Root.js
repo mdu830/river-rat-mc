db3cd6c0e6558dc8db57756e6f1d198d32cbb961
Mitch Duncan <mdu830@Mitchs-MacBook-Pro.local>
2023-06-19 10:54:26 -0400

    moved loading to root

--- client/src/Root.js
+++ client/src/Root.js
@@ -1,9 +1,35 @@
 import './App.css';
 import Header from './components/header'
 import { Outlet } from "react-router-dom";
-
+import { Spinner } from 'reactstrap';
+import { useEffect, useState } from 'react';
 
 function Root() {
+
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+
+    const onPageLoad = () => {
+      setIsLoading(false);
+    };
+
+    (document.readyState === 'complete') ?
+      onPageLoad() : window.addEventListener('load', onPageLoad);
+    return () => window.removeEventListener('load', onPageLoad);
+
+  }, []);
+
+  console.log(isLoading)
+
+  if (isLoading) {
+  return (
+    <div className='page'>
+      <Spinner id='spinner' color="secondary" />
+    </div>
+  )
+  }
+
   return (
     <div className="App">
       <Header />
--- client/src/routes/HomePage.js
+++ client/src/routes/HomePage.js
@@ -1,35 +1,10 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
 import '../assets/style.css'
-import { Spinner } from 'reactstrap';
 import logo from '../assets/images/logo.png'
 
 
 export default function HomePage() {
 
-  const [isLoading, setIsLoading] = useState(true)
-
-  useEffect(() => {
-
-    const onPageLoad = () => {
-      setIsLoading(false);
-    };
-
-    (document.readyState === 'complete') ?
-      onPageLoad() : window.addEventListener('load', onPageLoad);
-    return () => window.removeEventListener('load', onPageLoad);
-
-  }, []);
-
-  console.log(isLoading)
-
-  if (isLoading) {
-  return (
-    <div className='page'>
-      <Spinner id='spinner' color="secondary" />
-    </div>
-  )
-  }
   return (
     <>
       {/* all the other elements */}
