f3ccd45c76f768589193fe75198b5136d67a8e40
Mitch Duncan <mdu830@Mitchs-MacBook-Pro.local>
2023-06-19 11:22:21 -0400

    testing loading in home page

--- client/src/Root.js
+++ client/src/Root.js
@@ -1,44 +1,15 @@
 import './App.css';
 import Header from './components/header'
 import { Outlet } from "react-router-dom";
-import { Spinner } from 'reactstrap';
-import { Component, useEffect, useState } from 'react';
 
 function Root(data) {
 
-  const [isLoading, setIsLoading] = useState(true)
-
-  useEffect(() => {
-
-    const onPageLoad = () => {
-      setIsLoading(false);
-    };
-
-    (document.readyState === 'complete') 
-    ?
-    onPageLoad() 
-    : 
-    window.addEventListener('load', onPageLoad);
-    return () => window.removeEventListener('load', onPageLoad);
-
-  }, []);
-
-  console.log(data);
-
-  return isLoading
-    ?
-    (
-      <div className=''>
-        <Spinner id='spinner' color="secondary" />
-      </div>
-    )
-    :
-    (
-      <div className="App">
-        <Header />
-        <Outlet />
-      </div>
-    )
+  return (
+    <div className="App">
+      <Header />
+      <Outlet />
+    </div>
+  )
 }
 
 
--- client/src/routes/HomePage.js
+++ client/src/routes/HomePage.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import '../assets/style.css'
 import logo from '../assets/images/logo.png'
+import { Spinner } from 'reactstrap';
+import { Component, useEffect, useState } from 'react';
 
 
 export default function HomePage() {
 
-  return (
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+
+    const onPageLoad = () => {
+      setIsLoading(false);
+    };
+
+    (document.readyState === 'complete') 
+    ?
+    onPageLoad() 
+    : 
+    window.addEventListener('load', onPageLoad);
+    return () => window.removeEventListener('load', onPageLoad);
+
+  }, []);
+
+  return isLoading ?
+  (
+    <div className='page'>
+      <Spinner id='spinner' color="secondary" />
+    </div>
+  )
+  :
+  (
     <>
       {/* all the other elements */}
       <div className="page">
@@ -15,5 +41,6 @@ export default function HomePage() {
         </h2>
       </div>
     </>
-  );
+  )
+
 }
