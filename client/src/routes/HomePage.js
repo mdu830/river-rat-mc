484dc6b4b72f7a333dcfe7c9175c63de70a757c2
Mitch Duncan <mdu830@Mitchs-MacBook-Pro.local>
2023-06-19 21:10:50 -0400

    added container to haomepage

--- client/src/routes/HomePage.js
+++ client/src/routes/HomePage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../assets/style.css'
 import logo from '../assets/images/logo.png'
-import { Spinner } from 'reactstrap';
+import { Spinner, Container, Row, Col } from 'reactstrap';
 import { Component, useEffect, useState } from 'react';
 import Banner from '../components/Banner';
 
@@ -14,9 +14,14 @@ export default function HomePage() {
       {/* all the other elements */}
       <div id='HomePage' className="page">
         <Banner />
-        <h2>Welcome to River Rat Marine Construction!
-          Our website is currently under development.
-        </h2>
+        <Row>
+          <Container className="bg-dark p-2 customContainer ">
+            <h2>Welcome to River Rat Marine Construction! Our website is under development. </h2>
+          </Container>
+        </Row>
+
+
+
       </div>
     </>
   )
