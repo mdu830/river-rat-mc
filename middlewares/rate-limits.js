6ccc7d7c947386cae732cfcec2e3911baa11c6c4
Mitch Duncan <mdu830@Mitchs-MacBook-Pro.local>
2023-11-05 01:31:37 -0400

    updating middlewares

--- middlewares/rate-limits.js
+++ middlewares/rate-limits.js
@@ -2,8 +2,8 @@ import { rateLimit } from 'express-rate-limit'
 
 export const reqLimit = rateLimit({
 
-    windowMs: 24 * 60 * 60 * 1000, // 24 hrs in milliseconds
-    max: 20,
+    windowMs: 45 * 60 * 60 * 1000, // 24 hrs in milliseconds
+    max: 10,
     message: 'You have exceeded the 100 requests in 24 hrs limit!',
     standardHeaders: true,
     legacyHeaders: false
