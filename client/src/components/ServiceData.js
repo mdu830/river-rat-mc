74d3aa6fc739c3c02ca055e8e7fca11975865c4b
Mitch Duncan <mdu830@Mitchs-MacBook-Pro.local>
2023-11-09 16:15:36 -0500

    added services images (need missing image examples)

--- client/src/components/ServiceData.js
+++ client/src/components/ServiceData.js
@@ -1,53 +1,53 @@
-import GalleryImg from "./ImgGallery"
+import serviceImg from "./ServiceImg"
 
 const ServiceData = [
     {
         title: 'Docks and Piers',
         description: "Customized, designed and built to your specifications. So you can enjoy a leisurely stroll over the water, fishing, or access to your boathouse.",
-        Img1: GalleryImg[4].src,
-        Img2: GalleryImg[7].src,
+        Img1: serviceImg[4].src,
+        Img2: serviceImg[7].src,
     },
     {
         title: 'Seawalls & Bulkheads',
         description: "Seawalls or bulkheads help prevent soil erosion during storms while also maintaining your landscape. Available in sloppy V wood or concrete.",
-        Img1: GalleryImg[3].src,
-        Img2: GalleryImg[8].src
+        Img1: serviceImg[9].src,
+        Img2: serviceImg[8].src
     },
     {
         title: 'Boat Dock Repair',
         description: "Repair or maintain your existing dock due to storm or weather damage. Build a new dock or add an extension to any existing dock.",
-        Img1: GalleryImg[1].src,
-        Img2: GalleryImg[2].src
+        Img1: serviceImg[2].src,
+        Img2: serviceImg[5].src
     },
     {
         title: 'Decks',
         description: "Create an outdoor space to relax, take in the view of the water, entertain family and friends. Available in wood or concrete.",
-        Img1: GalleryImg[3].src,
-        Img2: GalleryImg[8].src
+        // Img1: serviceImg[3].src,
+        // Img2: serviceImg[8].src
     },
     {
         title: 'Bouthouses & Lifts',
         description: "Easily load and launch your boat with Tide Tamer boat lifts. While keeping them protected from the harsh weather when they are not being used.",
-        Img1: GalleryImg[3].src,
-        Img2: GalleryImg[8].src
+        Img1: serviceImg[0].src,
+        Img2: serviceImg[1].src
     },
     {
         title: 'Walkways & Stairs',
         description: "Enable easy access to your deck, dock or waterfront while adding safety and security with stainless steal screws and galvenized fasteners.",
-        Img1: GalleryImg[3].src,
-        Img2: GalleryImg[8].src
+        // Img1: serviceImg[3].src,
+        // Img2: serviceImg[8].src
     },
     {
         title: 'Rip Rap',
         description: "Help protect your investment by using loose stones to formand strengthen your seawall for an extra layer of prevention to soil erosion.",
-        Img1: GalleryImg[3].src,
-        Img2: GalleryImg[8].src
+        // Img1: serviceImg[3].src,
+        // Img2: serviceImg[8].src
     },
     {
         title: 'Site Preparation',
         description: "Heavy equipment usage for lot clearing including the removal of trees, bushes and rocks. Prepare land by grading and adding drainage for any water run off.",
-        Img1: GalleryImg[3].src,
-        Img2: GalleryImg[8].src
+        // Img1: serviceImg[3].src,
+        // Img2: serviceImg[8].src
     },
 ]
 
---
+++ client/src/components/ServiceImg.js
@@ -0,0 +1,96 @@
+import imgOne from '../assets/images/services/1000.png'
+import imgTwo from '../assets/images/services/2000.png'
+import imgThree from '../assets/images/services/3000.png'
+import imgFour from '../assets/images/services/4000.png'
+import imgFive from '../assets/images/services/5000.png'
+import imgSix from '../assets/images/services/6000.png'
+import imgSeven from '../assets/images/services/7000.png'
+import imgEight from '../assets/images/services/8000.png'
+import imgNine from '../assets/images/services/9000.png'
+import imgSixPointOne from '../assets/images/services/6001.png'
+
+const serviceImg = [
+    {
+        src: imgOne,
+        id: 'Sone',
+        height: 225,
+        key: 109009098098,
+        hash: '',
+        value: null
+    
+    },
+    {
+        src: imgTwo,
+        id: 'Stwo',
+        height: 225,
+        key: 2876876876,
+        hash: '',
+        value: null
+    },
+    {
+        src: imgThree,
+        id: 'Sthree',
+        height: 225,
+        key: 198798667575,
+        hash: '',
+        value: null
+    },
+    {
+        src: imgFour,
+        id: 'Sfour',
+        height: 169,
+        key: 45646345354,
+        hash: '',
+        value: null
+    },    
+    {
+        src: imgFive,
+        id: 'Sfive',
+        height: 169,
+        key: 8765455677878,
+        hash: '',
+        value: null
+    },
+    {
+        src: imgSix,
+        id: 'Ssix',
+        height: 132,
+        key: 6876543453232,
+        hash: '',
+        value: null
+    },   
+    {
+        src: imgSeven,
+        id: 'Sseven',
+        height: 480,
+        key: 987887765765,
+        hash: '',
+        value: null
+    },
+    {
+        src: imgEight,
+        id: 'Seight',
+        height: 400,
+        key: 675563225435,
+        hash: '',
+        value: null
+    },    
+    {
+        src: imgNine,
+        id: 'Snine',
+        height: 400,
+        key: 98766565435,
+        hash: '',
+        value: 'isLoaded'
+    },
+    {
+        src: imgSixPointOne,
+        id: 'Sndine',
+        height: 400,
+        key: 98766565435,
+        hash: '',
+        value: 'isLoaded'
+    },
+];
+
+export default serviceImg;
